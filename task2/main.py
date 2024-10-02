e7a02b9e5b93ee55a8e210273b9c4b38d8191952
Pokimonka <alenkaponomareva7@gmail.com>
2024-10-02 13:42:10 +0300

    files1

--- task2/main.py
+++ task2/main.py
@@ -2,34 +2,47 @@
 # для считывания файла и формирования json.object,
 #но я написала вот такой костыль)))
 cook_book = {}
-count_ingredients = 0
-inner_dict = []
-m_key = ""
-with open("recipe.txt", "r") as file1:
-    for line in file1:
-        strip_line = line.strip("\n")
-        if strip_line.isdigit() and type(int(strip_line)) == int:
-            count_ingredients = int(line.strip("\n"))
-            continue
-        if count_ingredients != 0:
-            tmp = strip_line.split("|")
-            in_d = {'ingredient_name': tmp[0], 'quantity': tmp[1], 'measure': tmp[2]}
-            inner_dict.append(in_d)
-            count_ingredients -= 1
-        elif type(strip_line) == str:
-            if strip_line != "":
-                m_key = strip_line
-            if len(inner_dict) != 0:
-                cook_book[m_key] = inner_dict
-                inner_dict = []
-    cook_book[m_key] = inner_dict
+def parse_file():
+    count_ingredients = 0
+    inner_dict = []
+    m_key = ""
+    with open("recipe.txt", "r") as file1:
+        for line in file1:
+            strip_line = line.strip("\n")
+            if strip_line.isdigit() and type(int(strip_line)) == int:
+                count_ingredients = int(line.strip("\n"))
+                continue
+            if count_ingredients != 0:
+                tmp = strip_line.split("|")
+                in_d = {'ingredient_name': tmp[0], 'quantity': tmp[1], 'measure': tmp[2]}
+                inner_dict.append(in_d)
+                count_ingredients -= 1
+            elif type(strip_line) == str:
+                if strip_line != "":
+                    m_key = strip_line
+                if len(inner_dict) != 0:
+                    cook_book[m_key] = inner_dict
+                    inner_dict = []
+        cook_book[m_key] = inner_dict
 
+#по заданию функция принимает только два значения, так что cook_book глобальная
 def get_shop_list_by_dishes(dishes, person_count):
-    result = {}
+    result = "{"
+    first = True
+    # Если есть другие варианты добавление в словарь одинаковых ключей - напишите пожалуйста
     for dish in dishes:
         for ingr in cook_book[dish]:
-            result[ingr['ingredient_name']] = {'measure': ingr['measure'], 'quantity': int(ingr['quantity'])*person_count}
+            tmp = {}
+            tmp[ingr['ingredient_name']] = {'measure': ingr['measure'], 'quantity': int(ingr['quantity'])*person_count}
+            if first:
+                result += "\n" + "\t" + str(tmp)
+                first = False
+            else:
+                result += "," + "\n" + "\t" + str(tmp)
+    result += "\n}"
+    return result
 
-    return dict(sorted(result.items()))
-
-print((get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)))
+if __name__ == '__main__':
+    parse_file()
+    print((get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)))
+    print((get_shop_list_by_dishes(['Утка по-пекински', 'Омлет'], 3)))
--- task3/main.py
+++ task3/main.py
@@ -1,22 +1,28 @@
 import os.path
 
-with open("files/1.txt", "r") as file1:
-    data_file1 = file1.read().splitlines()
+def read_files(directory) -> dict:
+    fds = sorted(os.listdir(directory))
+    lens_files = {}
+    for file in fds :
+        with open(directory + file, "r") as f:
+            data_file = f.read().splitlines()
+        lens_files[len(data_file)] = f
+    return lens_files
 
-with open("files/2.txt", "r") as file2:
-    data_file2 = file2.read().splitlines()
+def file_handler(lens_files) -> str:
+    result_text = ""
+    for length, file in sorted(lens_files.items()):
+        file = open(file.name)
+        filename = os.path.basename(file.name)
+        data = file.read().splitlines()
+        result_text += filename + "\n" + str(length) + "\n" + "\n".join(data) + "\n"
+        file.close()
+    return  result_text
 
-with open("files/3.txt", "r") as file3:
-    data_file3 = file3.read().splitlines()
-
-lens_files = {len(data_file1): file1, len(data_file2): file2, len(data_file3): file3}
-for length, file in sorted(lens_files.items()):
-    file = open(file.name)
-    filename = os.path.basename(file.name)
-    data = file.read().splitlines()
+if __name__ == '__main__':
+    dir = "files/"
+    len_file = read_files(dir)
+    text = file_handler(len_file) # можно функцию сделать void и она внутри будет записывать в файл
+    # но с расширением кода читаемость будет ухудшаться
     with open("files/result.txt", "a") as result_file:
-        result_file.write(filename + "\n")
-        result_file.write(str(length) + "\n")
-        result_file.write("\n".join(data) + "\n")
-    file.close()
-
+        result_file.write(text)
